71bd70d6e8e0cf5b74e10486f02240f9510eb3d0
Fabian Lopez <lombareload@gmail.com>
2015-09-21 22:18:57 -0500

    reached page 97 of living clojure

--- test.clj
+++ test.clj
@@ -21,19 +21,50 @@
 (take 10 (rest (range)))
 ;; (1 2 3 4 5 6 7 8 9 10)
 
+(defn factorial [n]
+  (loop [x n acum 1]
+    (if (= x 0) acum
+      (recur (dec x) (* x acum)))))
+(factorial 3)
+;; 6
+(factorial 4)
+;; 24
+
+(map #(str %1 \, %2) [\a \b \c] (range))
+;; ("a,0" "b,1" "c,2")
+
+(reduce str (take 4 (range)))
+;; "0123"
+
 ((complement empty?) [])
 ;; false
 
-(for [a [1 2] b [\a \b]]
+(remove even? [1 2 3 4 5 6])
+;; (1 3 5)
+
+(filter even? [1 2 3 4 5 6])
+;; (2 4 6)
+
+(filter (complement nil?) [1 2 nil 3 4])
+;; (1 2 3 4)
+
+
+(for [a [1 2] b [\a]]
   (str a \, b))
-;; ("1,a" "1,b" "2,a" "2,b")
+;; ("1,a" "2,a")
 
 (for [n '(0 1 2 3 4)
   :let [x (* n n)]
   :when (odd? n)] x)
 ;; (1 9)
 
+(vec '(1 2 3))
+;; [1 2 3]
+
 (into (sorted-map) {3 4, 2 3})
+;; {2 3, 3 4}
+
+(into (sorted-map) [[3 4] [2 3]])
 ;; {2 3, 3 4}
 
 (partition 2 (take 5 (range)))
@@ -47,6 +78,10 @@
 
 (def an-atom (atom 0))
 (swap! an-atom #(+ 1 %))
-(swap! an-atom #(+ 1 %))
+(swap! an-atom inc)
 @an-atom
 ;; 2
+
+(dotimes [i 6] (print i))
+;; stdout = 012345
+
